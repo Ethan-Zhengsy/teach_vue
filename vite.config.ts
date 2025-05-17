d306608965061d67f9fbf558255b9e4c7cc3969e
wwsjsjjs <896528971@qq.com>
2025-05-17 19:24:56 +0800

    修改配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 8080,  // 前端端口
+    host: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8088', // 后端地址
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, '') // 如有需要可开启
+      }
+    }
+  }
 })
